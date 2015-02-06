6cc92f2dc0fdc4dba15f6981c1d17468ef5a4c8b
Mineshopper <kmartshopper@gmail.com>
2015-02-06 14:13:54 -0800

    Parser code added, needs to be tested.

--- Lab 2/Lab1/Parser.cpp
+++ Lab 2/Lab1/Parser.cpp
@@ -25,30 +25,26 @@ Parser::~Parser()
 {
 }
 
-bool Parser::isNextType(Tokenizer::TokenType type)
+bool Parser::equals(Tokenizer::TokenType type)
 {
-    if (type == (pos + 1)->type)
-    {
-        ++pos;
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return type == pos->type();
 }
 
-bool Parser::isNextValue(char* input)
+bool Parser::equals(char* input)
 {
-    if (input == (pos + 1)->value)
-    {
-        ++pos;
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return input == pos->value();
+}
+
+void Parser::save()
+{
+    lastPos = pos;
+}
+
+// Restores position and returns false;
+bool Parser::failure()
+{
+    pos = lastPos;
+    return false;
 }
 
 bool Parser::parse(std::vector<Tokenizer::Token> tokens)
@@ -62,50 +58,126 @@ bool Parser::parse(std::vector<Tokenizer::Token> tokens)
 // FUNCTION → TYPE Identifier ( ARG_LIST ) COMPOUND_STMT
 bool Parser::function()
 {
+    save();
+
     if (type())
     {
-        if (isNextType(Tokenizer::Identifier))
+        ++pos;
+        if (equals(Tokenizer::Identifier))
         {
-            if (isNextValue("("))
+            ++pos;
+            if (equals("("))
             {
+                ++pos;
                 if (arg_list())
                 {
-                    if (isNextValue(")"))
+                    ++pos;
+                    if (equals(")"))
                     {
-                        return compound_stmt();
+                        ++pos;
+                        if (compound_stmt())
+                        {
+                            ++pos;
+                            return true;
+                        }
                     }
                 }
             }
         }
     }
 
-    return false;
+    return failure();
 }
 
-// ARG_LIST → ARG | ARG_LIST , ARG
+// ARG_LIST → ARG ARG_LIST2
 bool Parser::arg_list()
 {
+    save();
 
-    return false;
+    if (arg())
+    {
+        ++pos;
+        if (arg_list2())
+        {
+            ++pos;
+            return true;
+        }
+    }
+
+    return failure();
+}
+
+// ARG_LIST2 → , ARG ARG_LIST2 | λ
+bool Parser::arg_list2()
+{
+    save();
+
+    if (equals(","))
+    {
+        ++pos;
+        if (arg())
+        {
+            ++pos;
+            if (arg_list2())
+            {
+                ++pos;
+                return true;
+            }
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // ARG → TYPE Identifier
 bool Parser::arg()
 {
+    save();
 
-    return false;
+    if (type())
+    {
+        ++pos;
+        if (equals(Tokenizer::Identifier))
+        {
+            ++pos;
+            return true;
+        }
+    }
+
+    return failure();
 }
 
 // DECLARATION → TYPE IDENT_LIST ;
 bool Parser::declaration()
 {
+    save();
 
-    return false;
+    if (type())
+    {
+        ++pos;
+        if (ident_list())
+        {
+            ++pos;
+            if (equals(";"))
+            {
+                ++pos;
+                return true;
+            }
+        }
+    }
+
+    return failure();
 }
 
 // TYPE → int | float
 bool Parser::type()
 {
+    if (pos->type() == Tokenizer::Integer || pos->type() == Tokenizer::Float)
+    {
+        ++pos;
+        return true;
+    }
 
     return false;
 }
@@ -113,104 +185,486 @@ bool Parser::type()
 // IDENT_LIST → Identifier , IDENT_LIST Identifier
 bool Parser::ident_list()
 {
+    save();
 
-    return false;
+    if (equals(Tokenizer::Identifier))
+    {
+        ++pos;
+        if (equals(","))
+        {
+            ++pos;
+            if (ident_list())
+            {
+                ++pos;
+                if (equals(Tokenizer::Identifier))
+                {
+                    ++pos;
+                    return true;
+                }
+            }
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // STATEMENT → FOR_STMT | WHILE_STMT | EXPRESSION ; | IF_STMT | COMPOUND_STMT | DECLARATION | ;
 bool Parser::statement()
 {
+    save();
 
-    return false;
+    if (for_stmt())
+    {
+        ++pos;
+        return true;
+    }
+    else if (while_stmt())
+    {
+        ++pos;
+        return true;
+    }
+    else if (expression())
+    {
+        ++pos;
+        if (equals(";"))
+        {
+            ++pos;
+            return true;
+        }
+    }
+    else if (if_stmt())
+    {
+        ++pos;
+        return true;
+    }
+    else if (compound_stmt())
+    {
+        ++pos;
+        return true;
+    }
+    else if (declaration())
+    {
+        ++pos;
+        return true;
+    }
+    else if (pos->value() == ";")
+    {
+        ++pos;
+        return true;
+    }
+
+    return failure();
 }
 
 // FOR_STMT → for ( EXPRESSION ; OPT_EXPR ; OPT_EXPR ) STATEMENT
 bool Parser::for_stmt()
 {
+    save();
 
-    return false;
+    if (equals("for"))
+    {
+        ++pos;
+        if (equals("("))
+        {
+            ++pos;
+            if (expression())
+            {
+                ++pos;
+                if (equals(";"))
+                {
+                    ++pos;
+                    if (opt_expr())
+                    {
+                        ++pos;
+                        if (equals(";"))
+                        {
+                            ++pos;
+                            if (opt_expr())
+                            {
+                                ++pos;
+                                if (equals(")"))
+                                {
+                                    ++pos;
+                                    if (statement())
+                                    {
+                                        ++pos;
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return failure();
 }
 
 // OPT_EXPR → EXPRESSION | λ
 bool Parser::opt_expr()
 {
+    if (expression())
+    {
+        ++pos;
+        return true;
+    }
 
-    return false;
+    return true; // λ
 }
 
 // WHILE_STMT → while ( EXPRESSION ) STATEMENT
 bool Parser::while_stmt()
 {
+    save();
 
-    return false;
+    if (equals("while"))
+    {
+        ++pos;
+        if (equals("("))
+        {
+            ++pos;
+            if (expression())
+            {
+                ++pos;
+                if (equals(")"))
+                {
+                    ++pos;
+                    if (statement())
+                    {
+                        ++pos;
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    return failure();
 }
 
 // IF_STMT → if ( EXPRESSION ) STATEMENT ELSEPART
 bool Parser::if_stmt()
 {
+    save();
 
-    return false;
+    if (equals("if"))
+    {
+        ++pos;
+        if (equals("("))
+        {
+            ++pos;
+            if (expression())
+            {
+                ++pos;
+                if (equals(")"))
+                {
+                    ++pos;
+                    if (statement())
+                    {
+                        ++pos;
+                        if (elsepart())
+                        {
+                            ++pos;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return failure();
 }
 
 // ELSEPART → else STATEMENT | λ
 bool Parser::elsepart()
 {
+    save();
 
-    return false;
+    if (equals("else"))
+    {
+        ++pos;
+        if (statement())
+        {
+            ++pos;
+            return true;
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // COMPOUND_STMT → { STMT_LIST }
 bool Parser::compound_stmt()
 {
+    save();
 
-    return false;
+    if (equals("{"))
+    {
+        ++pos;
+        if (stmt_list())
+        {
+            ++pos;
+            if (equals("}"))
+            {
+                ++pos;
+                return true;
+            }
+        }
+    }
+
+    return failure();
 }
 
-// STMT_LIST → STMT_LIST STATEMENT | λ
+// STMT_LIST → STATEMENT STMT_LIST | λ
 bool Parser::stmt_list()
 {
+    save();
 
-    return false;
+    if (statement())
+    {
+        ++pos;
+        if (stmt_list())
+        {
+            ++pos;
+            return true;
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // EXPRESSION → Identifier = EXPRESSION | RVALUE
 bool Parser::expression()
 {
+    save();
 
-    return false;
+    if (equals(Tokenizer::Identifier))
+    {
+        ++pos;
+        if (equals("="))
+        {
+            ++pos;
+            if (expression())
+            {
+                ++pos;
+                return true;
+            }
+        }
+    }
+    else if (rvalue())
+    {
+        ++pos;
+        return true;
+    }
+
+    return failure();
 }
 
-// RVALUE → RVALUE COMPARE MAG | MAG
+// RVALUE → MAG RVALUE2
 bool Parser::rvalue()
 {
+    save();
 
-    return false;
+    if (mag())
+    {
+        ++pos;
+        if (rvalue2())
+        {
+            ++pos;
+            return true;
+        }
+    }
+
+    return failure();
+}
+
+// RVALUE2 → COMPARE MAG RVALUE2 | λ
+bool Parser::rvalue2()
+{
+    save();
+
+    if (compare())
+    {
+        ++pos;
+        if (mag())
+        {
+            ++pos;
+            if (rvalue2())
+            {
+                ++pos;
+                return true;
+            }
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // COMPARE → == | < | > | <= | >= | !=
 bool Parser::compare()
 {
+    save();
 
-    return false;
+    if (equals("=="))
+    {
+        ++pos;
+        return true;
+    }
+    else if (equals("<"))
+    {
+        ++pos;
+        return true;
+    }
+    else if (equals(">"))
+    {
+        ++pos;
+        return true;
+    }
+    else if (equals("<="))
+    {
+        ++pos;
+        return true;
+    }
+    else if (equals(">="))
+    {
+        ++pos;
+        return true;
+    }
+    else if (equals("!="))
+    {
+        ++pos;
+        return true;
+    }
+
+    return failure();
 }
 
-// MAG → MAG + TERM | MAG - TERM | TERM
+// MAG → TERM MAG2
 bool Parser::mag()
 {
+    save();
 
-    return false;
+    if (term())
+    {
+        ++pos;
+        if (mag2())
+        {
+            ++pos;
+            return true;
+        }
+    }
+
+    return failure();
 }
 
-// TERM → TERM * FACTOR | TERM / FACTOR | FACTOR
+// MAG2 → + TERM MAG2 | - TERM MAG2 | λ
+bool Parser::mag2()
+{
+    save();
+
+    if (equals("+") || equals("-"))
+    {
+        ++pos;
+        if (term())
+        {
+            ++pos;
+            if (mag2())
+            {
+                ++pos;
+                return true;
+            }
+        }
+        return failure();
+    }
+
+    return true; // λ
+}
+
+// TERM → FACTOR TERM2
 bool Parser::term()
 {
+    save();
 
-    return false;
+    if (factor())
+    {
+        ++pos;
+        if (term2())
+        {
+            ++pos;
+            return true;
+        }
+    }
+
+    return failure();
+}
+
+// TERM2 → * FACTOR TERM2 | / FACTOR TERM2 | λ
+bool Parser::term2()
+{
+    save();
+
+    if (equals("*") || equals("/"))
+    {
+        ++pos;
+        if (factor())
+        {
+            ++pos;
+            if (term2())
+            {
+                ++pos;
+                return true;
+            }
+        }
+        return failure();
+    }
+
+    return true; // λ
 }
 
 // FACTOR → ( EXPRESSION ) | - FACTOR | + FACTOR | Identifier | TYPE
 bool Parser::factor()
 {
+    save();
 
-    return false;
+    if (equals("("))
+    {
+        ++pos;
+        if (expression())
+        {
+            ++pos;
+            if (equals(")"))
+            {
+                ++pos;
+                return true;
+            }
+        }
+    }
+    else if (equals("-") || equals("+"))
+    {
+        ++pos;
+        if (factor())
+        {
+            ++pos;
+            return true;
+        }
+    }
+    else if (equals(Tokenizer::Identifier))
+    {
+        ++pos;
+        return true;
+    }
+    else if (type())
+    {
+        ++pos;
+        return true;
+    }
+
+    return failure();
 }
--- Lab 2/Lab1/Parser.h
+++ Lab 2/Lab1/Parser.h
@@ -34,8 +34,11 @@ class Parser
         // FUNCTION → TYPE Identifier ( ARG_LIST ) COMPOUND_STMT
         bool function();
 
-        // ARG_LIST → ARG | ARG_LIST , ARG
+        // ARG_LIST → ARG ARG_LIST2
         bool arg_list();
+
+        // ARG_LIST2 → , ARG ARG_LIST2 | λ
+        bool arg_list2();
 
         // ARG → TYPE Identifier
         bool arg();
@@ -70,35 +73,56 @@ class Parser
         // COMPOUND_STMT → { STMT_LIST }
         bool compound_stmt();
 
-        // STMT_LIST → STMT_LIST STATEMENT | λ
+        // STMT_LIST → STATEMENT STMT_LIST | λ
         bool stmt_list();
 
         // EXPRESSION → Identifier = EXPRESSION | RVALUE
         bool expression();
 
-        // RVALUE → RVALUE COMPARE MAG | MAG
+        // RVALUE → MAG RVALUE2
         bool rvalue();
+
+        // RVALUE2 → COMPARE MAG RVALUE2 | λ
+        bool rvalue2();
 
         // COMPARE → == | < | > | <= | >= | !=
         bool compare();
 
-        // MAG → MAG + TERM | MAG - TERM | TERM
+        // MAG → TERM MAG2
         bool mag();
 
-        // TERM → TERM * FACTOR | TERM / FACTOR | FACTOR
+        // MAG2 → + TERM MAG2 | - TERM MAG2 | λ
+        bool mag2();
+
+        // TERM → FACTOR TERM2
         bool term();
+
+        // TERM2 → * FACTOR TERM2 | / FACTOR TERM 2 | λ
+        bool term2();
 
         // FACTOR → ( EXPRESSION ) | - FACTOR | + FACTOR | Identifier | TYPE
         bool factor();
 
-        // Helper function that returns true if equality exists, while also incrementing iterator
-        bool isNextType(Tokenizer::TokenType type);
+        // Helper function that returns true if equality exists at current position, incrementing iterator if true
+        bool equals(Tokenizer::TokenType type);
 
-        // Helper function that returns true if equality exists, while also incrementing iterator
-        bool isNextValue(char* input);
+        // Helper function that returns true if equality exists at current position, incrementing iterator if true
+        bool equals(char* input);
+
+        // Returns true.
+        bool success();
+
+        // Restores position and returns false;
+        bool failure();
+
+        // Saves position
+        void save();
 
         // Stores current position in tokens vector
         std::vector<Tokenizer::Token>::iterator pos;
+
+        // Temporary object for position
+        std::vector<Tokenizer::Token>::iterator lastPos;
 
 };
 
